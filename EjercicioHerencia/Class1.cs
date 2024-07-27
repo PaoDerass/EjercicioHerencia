1c7f45e2a2ff0ffc329e8807d4f86654273e685f
PaoDerass <paola_deras@unitec.edu>
2024-07-26 20:46:40 -0600

    Creacion de clases

---
+++ EjercicioHerencia/Class1.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace EjercicioHerencia
+{
+    internal class Class1 : Operaciones
+
+    {
+        public void Imprimir(double radio, double lado)
+        {
+            double circulo = AreaCirculo(radio);
+            double Cubo = VolCubo(lado);
+            double circunferencia = LonCircuferencia(radio);
+
+            Console.WriteLine($"Area del circulo: {AreaCirculo}")
+        }
+    }
+}
---
+++ EjercicioHerencia/Operaciones.cs
@@ -0,0 +1,19 @@
+﻿
+namespace EjercicioHerencia
+{
+    internal class Operaciones
+    {
+        public double AreaCirculo (double radio)
+        {
+            return Math.PI * Math.Pow(radio, 2);
+        }
+        public double VolCubo(double lado)
+        {
+            return Math.Pow(lado, 3);
+        }
+        public double LonCircuferencia(double radio)
+        {
+            return 2 * Math.PI * radio;
+        }
+    }
+}
